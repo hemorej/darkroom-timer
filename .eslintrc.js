fc78c10434962f0cbcc1f13741c18aeb52be4d0f
j_a_ <jerome.arfouche@gmail.com>
2022-01-18 11:31:04 -0500

    big upgrades to resolve js dep issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,27 +2,23 @@ module.exports = {
   root: true,
 
   env: {
-    node: true
+    node: true,
   },
 
-  extends: ["plugin:vue/essential"],
+  extends: ['plugin:vue/essential'],
 
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
 
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: 'babel-eslint',
   },
 
   globals: {
-    "_": true
+    _: true,
   },
 
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/prettier'
-  ]
-};
+  extends: ['plugin:vue/essential', 'eslint:recommended', '@vue/prettier'],
+}

6393ef28ec8456e37789be271efce3c671c042f7
j_a_ <jerome.arfouche@gmail.com>
2022-01-19 18:31:41 -0500

    Revert "big upgrades to resolve js dep issues"
    This reverts commit fc78c10434962f0cbcc1f13741c18aeb52be4d0f.

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  publicPath: 'darkroom',
-  chainWebpack: (config) => {
-    config.module.rule('eslint').use('eslint-loader').options({
-      fix: true,
-    })
-  },
+  'publicPath' : 'darkroom',
+   chainWebpack: config => {
+     config.module.rule('eslint').use('eslint-loader').options({
+       fix: true
+     })
+   }
 }
+

7d44e662198dcd92575925c236aa7a18decde7a9
j_a_ <jerome.arfouche@gmail.com>
2024-06-18 08:49:56 -0400

    upgrade eslint config

---
+++ eslintrc.mjs
@@ -0,0 +1,34 @@
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  ...compat.extends(
+    "plugin:vue/essential",
+    "eslint:recommended"
+  ),
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        _: true,
+      },
+    },
+    ignores: ["dist/*", "node_modules/*"],
+    rules: {
+      "no-console": "off",
+      "no-debugger": "off",
+      "vue/no-multiple-template-root": "off",
+    },
+  },
+];

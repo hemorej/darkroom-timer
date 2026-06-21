104956b6733b2db23a178b65efb61bb73d1b018e
j_a_ <jerome.arfouche@pm.me>
2026-06-21 17:01:56 -0400

    add infra dir for config backup

--- infra/forge_deploy.sh
+++ infra/forge_deploy.sh
@@ -1,7 +1,8 @@
 cd $FORGE_SITE_PATH
 
-git pull origin $FORGE_SITE_BRANCH
-npm install --no-audit --no-fund --silent
+git checkout . # undo temp changes, restore to last git status to avoid pull conflicts
+git pull origin $FORGE_SITE_BRANCH # get latest
+npm install --no-audit --no-fund --silent 
 npm install @rollup/rollup-linux-x64-gnu --silent --no-audit --no-fund # bug in optional dependencies
 npm run build
 

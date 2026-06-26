aa451e71c3923745dd2942be11baf75f408ea32e
j_a_ <jerome.arfouche@pm.me>
2026-06-26 16:13:06 -0400

    remove symlink on deploy

--- infra/forge_deploy.sh
+++ infra/forge_deploy.sh
@@ -5,7 +5,3 @@ git pull origin $FORGE_SITE_BRANCH # get latest
 npm install --no-audit --no-fund --silent 
 npm install @rollup/rollup-linux-x64-gnu --silent --no-audit --no-fund # bug in optional dependencies
 npm run build
-
-if [ ! -L "$LINK_NAME" ]; then
-    ln -s "$TARGET" "$LINK_NAME"
-fi

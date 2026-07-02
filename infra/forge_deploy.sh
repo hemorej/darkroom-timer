206888daf3f0328bc01bdaa186ce864382075a31
j_a_ <jerome.arfouche@pm.me>
2026-07-02 18:17:02 -0400

    bugfix in deploy

--- infra/forge_deploy.sh
+++ infra/forge_deploy.sh
@@ -1,6 +1,8 @@
+# npm install @rollup/rollup-linux-x64-gnu --silent --no-audit --no-fund # bug in optional dependencies
+
 cd $FORGE_SITE_PATH
 
 git checkout . # undo temp changes, restore to last git status to avoid pull conflicts
 git pull origin $FORGE_SITE_BRANCH # get latest
-pnpm install --frozen-lockfile
-pnpm run build
+/home/forge/.local/share/pnpm/bin/pnpm install --frozen-lockfile
+/home/forge/.local/share/pnpm/bin/pnpm run build
